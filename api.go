d2c4312918ce7a05c09f8f8cf3518af04d5c15f7
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-04-19 16:24:30 +0545

    Initial commit

---
+++ api.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr  string
+	store Store
+}
+
+func NewAPIServer(addr string, store Store) *APIServer {
+	return &APIServer{
+		addr:  addr,
+		store: store,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+	subRouter := router.PathPrefix("/api/v1").Subrouter()
+
+	log.Println("starting the API server at", s.addr)
+
+	log.Fatal(http.ListenAndServe(s.addr, subRouter))
+}
